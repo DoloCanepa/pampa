f5895fed42e6116819660d1dcb7a6a115184ba6b
Dolores <lolacanepa@gmail.com>
2023-02-10 12:46:54 -0300

    “encabez”

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 const hamburguesa = document.querySelector('.hamburguesa');
 const navegacion = document.querySelector('.navegacion');
+const enlaces = document.querySelectorAll('.navegacion a');
 
 document.addEventListener('DOMContentLoaded',()=>{
     mostrarMenu();
+    cerrarMenu();
 });
 
 function mostrarMenu(){
@@ -10,4 +12,17 @@ function mostrarMenu(){
         navegacion.classList.toggle('ocultar');
 
     }); 
+}
+
+function cerrarMenu(){
+    enlaces.forEach(enlace =>{
+        enlace.addEventListener('click',(e)=>{
+            e.preventDefault();
+            const seccion = document.querySelector(e.target.attributes.href.value);
+            cambioSeccion(seccion);
+            if(e.target.tagName === 'A'){
+                navegacion.classList.add('ocultar');
+            }
+        });
+    });
 }
