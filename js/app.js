c3c94cc51b55ebb070281057e7e05cc4943b0153
Dolores <lolacanepa@gmail.com>
2023-02-17 00:41:34 -0300

    “acordeon”

--- js/app.js
+++ js/app.js
@@ -1,28 +1,44 @@
 const hamburguesa = document.querySelector('.hamburguesa');
 const navegacion = document.querySelector('.navegacion');
 const enlaces = document.querySelectorAll('.navegacion a');
+const jobs = document.querySelector('.jobs');
+const contenido = document.querySelector('.contenido');
+const news = document.querySelector('.news');
+const contenidonews = document.querySelector('.contenidonews');
 
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
     mostrarMenu();
     cerrarMenu();
+
 });
 
-function mostrarMenu(){
-    hamburguesa.addEventListener('click',()=>{
+function mostrarMenu() {
+    hamburguesa.addEventListener('click', () => {
         navegacion.classList.toggle('ocultar');
 
-    }); 
+    });
 }
 
-function cerrarMenu(){
-    enlaces.forEach(enlace =>{
-        enlace.addEventListener('click',(e)=>{
+function cerrarMenu() {
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', (e) => {
             e.preventDefault();
             const seccion = document.querySelector(e.target.attributes.href.value);
             cambioSeccion(seccion);
-            if(e.target.tagName === 'A'){
+            if (e.target.tagName === 'A') {
                 navegacion.classList.add('ocultar');
             }
         });
     });
 }
+
+//acordeon//
+jobs.addEventListener('click', function() {
+    contenido.classList.toggle('desplegado');
+});
+
+news.addEventListener('click', function() {
+    contenidonews.classList.toggle('desplegado');
+});
+
+
